78abd2c785f71b514ef825bd9464f0f3a2193dca
ImJcm <wjsckdals0909@naver.com>
2023-06-27 18:43:01 +0900

    @ManyToOne 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -1,3 +1,27 @@
+//Ver. ManyToOne 단방향 & 양방향
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
+/*
+//Ver. OneToOne 단방향 & 양방향
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
@@ -18,4 +42,4 @@ public class Food {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
-}
+}*/
--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -1,3 +1,22 @@
+//ManyToOne 단방향 & 양방향
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
+/*
+//OneToOne 단방향 & 양방향
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
@@ -21,4 +40,4 @@ public class User {
         this.food = food;
         food.setUser(this);
     }
-}
+}*/
